a21ccee86c81cbe73dd1f79368718284a5034d85
Alejandra Izquierdo <pilarale95@gmail.com>
2018-10-13 21:34:28 -0500

    Se pueden conectar varios usuarios y cuando se desconectan se eliminan.

--- js/game.js
+++ js/game.js
@@ -32,20 +32,15 @@ var die;
 var collect;
 
 Game.create = function (){
-  //Add random stars
-    //game.add.sprite((Math.random()*800)+1, (Math.random()*600)+1, 'star');
+  Game.playerMap = {};
 
-	//  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
 
     music = game.add.audio('bgmusic');
     die = game.add.audio('die');
     collect = game.add.audio('collect');
-
-    //game.sound.setDecodedCallback([ die, collect ], start, this);
 
     music.play();
     //  The platforms group contains the ground and the 2 ledges we can jump on
@@ -71,26 +66,8 @@ Game.create = function (){
     ledge = platforms.create(-150, 250, 'ground');
 
     ledge.body.immovable = true;
-	//Here I can create another ledge
-	//ledge = platforms.create(200, 300, 'ground');
-	//ledge.scale.setTo(.5, 2);
-    //ledge.body.immovable = true;
 
-	// The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.5;
-    player.body.gravity.y = 500;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
+    Client.askNewPlayer();
 
     //The BADDIE and its settings
     baddie = game.add.sprite((Math.random()*700)+80, game.world.height - 96, 'baddie');
@@ -127,97 +104,32 @@ Game.create = function (){
     lifeText = game.add.text(220, 16, 'lifes: 3', {fontSize: '32px', fill: '#f83800'});
 
 	//  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-}
+    //cursors = game.input.keyboard.createCursorKeys();
+    layer.inputEnabled = true;
+    layer.events.onInputUp.add(Game.getCoordinates, this);
 
-Game.update = function () {
-	//  Collide the player and the stars with the platforms
-  var hitPlatform =  game.physics.arcade.collide(player, platforms);
-	game.physics.arcade.collide(stars, platforms);
-  //game.physics.arcade.collide(player, baddie);
-
-	//  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-    game.physics.arcade.overlap(player, baddie, baddieOverlap, null, this);
-
-	//  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-    baddie.body.velocity.x = 0;//This stop sliding
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
-    }
-    //&& player.body.touching.down && hitPlatform
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown)
-    {
-        player.body.velocity.y = -350;
-    }
-
-    if (flag) {
-      baddie.body.velocity.x = 200;
-      baddie.animations.play('right');
-      if (baddie.x == game.world.width -32) {
-        flag = false;
-      }
-    }else {
-      baddie.body.velocity.x = -200;
-      baddie.animations.play('left');
-      if (baddie.x == 0) {
-        flag = true;
-      }
-    }
-}
-
-function collectStar (player, star) {
-
-    // Removes the star from the screen
-    star.kill();
-
-	//  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-    collect.play();
-    if (score == 120) {
-      music.pause();
-      alert('You won the game, congratulations!');
-      location.reload();
-    }
 
 }
 
-function baddieOverlap(player, baddie) {
+Game.addNewPlayer = function (id, x, y) {
 
-  player.kill();
-  baddie.kill();
-
-  player.reset(32, game.world.height - 150);
-  baddie.reset((Math.random()*700)+80, game.world.height - 96);
-  flag = true;
-  lifeCount -= 1;
-  lifeText.text = 'Lives: ' + lifeCount;
-  die.play();
-  if (lifeCount == 0) {
-    music.pause();
-    alert('You lose :(');
-    location.reload();
-  }
+  Game.playerMap[id] = game.add.sprite(x,y,'dude');
 }
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
+
+Game.getCoordinates = function(layer,pointer){
+    Client.sendClick(pointer.worldX,pointer.worldY);
+};
+
+Game.movePlayer = function(id,x,y){
+    var player1 = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var duration = distance*10;
+    var tween = game.add.tween(player);
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
--- server.js
+++ server.js
@@ -15,29 +15,40 @@ server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
 });
 
-// server.lastPlayderID = 0; // Keep track of the last id assigned to a new player
-//
-// io.on('connection',function(socket){
-//     socket.on('newplayer',function(){
-//         socket.player = {
-//             id: server.lastPlayderID++,
-//             x: randomInt(32, game.world.width -32),
-//             y: game.world.height
-//         };
-//         socket.emit('allplayers',getAllPlayers());
-//         socket.broadcast.emit('newplayer',socket.player);
-//     });
-// });
-//
-// function getAllPlayers(){
-//     var players = [];
-//     Object.keys(io.sockets.connected).forEach(function(socketID){
-//         var player = io.sockets.connected[socketID].player;
-//         if(player) players.push(player);
-//     });
-//     return players;
-// }
-//
-// function randomInt (low, high) {
-//     return Math.floor(Math.random() * (high - low) + low);
-// }
+server.lastPlayderID = 0; // Keep track of the last id assigned to a new player
+
+io.on('connection',function(socket){
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(32, 600),
+            y: 488
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
+
+        socket.on('click',function(data){
+            console.log('click to '+data.x+', '+data.y);
+            socket.player.x = data.x;
+            socket.player.y = data.y;
+            io.emit('move',socket.player);
+        });
+
+        socket.on('disconnect',function(){
+            io.emit('remove',socket.player.id);
+        });
+    });
+});
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
